ebf8ce569f3774194c2f7fe7cf4680598b182d70
Mike Chau <mchau@iwu.edu>
2013-10-27 22:41:06 -0500

    Everything works

--- mercury-carbon.js
+++ mercury-carbon.js
@@ -1,19 +1,91 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to mercury-carbon.";
-  };
+Emails = new Meteor.Collection("emails");
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+// Github account usernames of admin users
+var ADMIN_USERS = ['mikechau'];
+function isAdmin(userId) {
+  var user = Meteor.users.findOne({_id: userId});
+  try {
+    return ADMIN_USERS.indexOf(user.services.github.username) !== -1;
+  } catch(e) {
+    return false;
+ }
+}
+  
+if (Meteor.isClient) {
+
+  Meteor.subscribe('userData');
+  Meteor.subscribe('emails');
+  Template.footer.events({
+    'click .login' : function(evt, tmpl){
+      Meteor.loginWithGithub();
+      return false;
+    },
+
+    'click .admin' : function(evt, tmpl){
+      Session.set("showAdmin", !Session.get("showAdmin"));
+    }
+   });
+
+  Template.signup.events({
+    'submit form' : function (evt, tmpl) {
+
+      var email = tmpl.find('input').value,
+          doc = { email: email,
+                  referrer: document.referrer,
+                  timestamp: new Date()
+                };
+
+      if (EMAIL_REGEX.test(email)){
+        Session.set("showBadEmail", false);
+        Meteor.call("insertEmail", doc);
+        Session.set("emailSubmitted", true);
+      } else {
+        Session.set("showBadEmail", true);
+      }
+      return false;
     }
   });
+
+  Template.signup.showBadEmail = function () {
+    return Session.get("showBadEmail");
+  };
+
+  Template.signup.emailSubmitted = function () {
+    return Session.get("emailSubmitted");
+  };
+
+  Template.footer.isAdmin = function() {
+    return isAdmin(Meteor.userId());
+  };
+
+  Template.main.showAdmin = function() {
+    return Session.get("showAdmin");
+  };
+
+  Template.admin.emails = function() {
+    return Emails.find().fetch();
+  };
+
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  
+  Meteor.publish("userData", function () {
+    return Meteor.users.find({_id: this.userId},
+      {fields: {'services.github.username': 1, 'username':1}});
+  });
+
+  Meteor.publish("emails", function() {
+    if (isAdmin(this.userId)) {
+      return Emails.find();
+    }
+  });
+  
+  Meteor.methods({
+    insertEmail: function(doc) {
+      Emails.insert(doc);
+    }
   });
 }
